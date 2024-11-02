938a31fa8a4ce334c006b903037ab47ccd9995c9
Parnni <prajapaticamps8976@gmail.com>
2024-11-02 16:16:21 +0530

    Create a scheduled task

--- src/cfehome/celery.py
+++ src/cfehome/celery.py
@@ -7,3 +7,12 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cfehome.settings')
 app = Celery('cfehome')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+
+CELERY_BEAT_SCHEDULE = {
+    'populate_temperature': {
+        'task': 'sensors.tasks.populate_temperature_values',
+        'schedule': 5,
+    }
+}
+
+app.conf.beat_schedule = CELERY_BEAT_SCHEDULE
