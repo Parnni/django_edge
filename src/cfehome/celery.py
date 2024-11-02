995dd0885fe12dd06532a52aee7a00bbc46418e3
Parnni <prajapaticamps8976@gmail.com>
2024-11-02 19:01:49 +0530

    Create seperate queues for each node

--- src/cfehome/celery.py
+++ src/cfehome/celery.py
@@ -1,6 +1,7 @@
 import os
 
 from celery import Celery
+from kombu import Exchange, Queue
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cfehome.settings')
 
@@ -8,11 +9,20 @@ app = Celery('cfehome')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
-CELERY_BEAT_SCHEDULE = {
-    'populate_temperature': {
+# Setting up individual queue and beat schedule for each node.
+CELERY_TASK_QUEUES = []
+CELERY_BEAT_SCHEDULE = {}
+
+nodes = ['node-1', 'node-2']
+for node in nodes:
+    CELERY_TASK_QUEUES.append(Queue(node, Exchange(node), routing_key=node))
+    CELERY_BEAT_SCHEDULE[f'populate_temperature_{node}'] = {
         'task': 'sensors.tasks.populate_temperature_values',
         'schedule': 5,
+        'options': {
+            'queue': node,
+        },
     }
-}
 
+app.conf.task_queues = CELERY_TASK_QUEUES
 app.conf.beat_schedule = CELERY_BEAT_SCHEDULE
